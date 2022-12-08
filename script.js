ed5465c544cb73338166704ade20b6d8e524c8e6
Rezeda <dinievarezeda@mail.ru>
2022-12-08 23:38:31 +0300

    выполнено усложненное задание

--- script.js
+++ script.js
@@ -9,28 +9,48 @@ function numInMind() {
 
 let displayNum = numInMind(); // displayNum();
 
+function steps() {
+    let num = 10;
+    return function(){
+        return num;
+    };
+}
+
+let lessSteps = steps();
+
 // Программа должна быть выполнена с помощью рекурсии, без единого цикла.
 const isNumber = function(num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-function f1(num){
-    let ask = prompt("Угадай число от 1 до 100");
-
-    if (ask === null){
-        alert("Игра окончена");
-    } else if (ask > num ) {
-        alert("Загаданное число меньше");
-        f1(num);
-    } else if (ask < num){
-        alert("Загаданное число больше");
-        f1(num);
-    } else if (ask == num) {
-        alert("Поздравляю, Вы угадали!!!");
-    } else if (ask !== isNumber(num)){
-        alert("Введи число!");
-        f1(num);
+function f1(num, q){
+    if (q > 0) {
+        let ask = prompt("Угадай число от 1 до 100");
+        q--;
+        if (ask === null){
+            alert("Игра окончена");
+        } else if (ask > num ) {
+            alert("Загаданное число меньше, осталось попыток " + q);
+            f1(num, q);
+        } else if (ask < num){
+            alert("Загаданное число больше, осталось попыток " + q);
+            f1(num, q);
+        } else if (ask == num) {
+            let answer = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+            if (answer) {
+                return f1(num, q=10);
+            }
+        } else if (ask !== isNumber(num)){
+            alert("Введи число!");
+            f1(num, q);
+        }
+    }
+    if (q === 0) {
+        let answer = confirm("Попытки закончились, хотите сыграть еще?");
+        if (answer){
+           return f1(num, q=10);
+        }
     }
 }
 
-f1(displayNum());
+f1(displayNum(), lessSteps());
